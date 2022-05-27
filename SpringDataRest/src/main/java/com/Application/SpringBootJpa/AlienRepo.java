8a616c56406ff0cc5d10381dba3e5eff156210ab
Usha R <Usha R@Hyd-ShruthiM>
2022-05-27 00:07:25 -0400

    SpringDataRest

---
+++ SpringDataRest/src/main/java/com/Application/SpringBootJpa/AlienRepo.java
@@ -0,0 +1,11 @@
+package com.Application.SpringBootJpa;
+
+import com.Application.SpringBootJpa.model.Alien;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "aliens", path = "aliens")
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
+
+}
